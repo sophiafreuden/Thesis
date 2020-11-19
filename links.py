4bf33dfe6bfbc423185de85b015f583c2b12ad04
sophiafreuden <sfreuden@g.harvard.edu>
2020-11-19 16:59:58 -0500

    Commit with script with English link crawler slightly upgraded. Will need to figure out how to add links to a list with out duplicating them.

--- links.py
+++ links.py
@@ -13,6 +13,18 @@ raw = requests.get("https://www.rt.com/search?q=russia+soros")
 
 soup = BeautifulSoup(raw.content, "html.parser")
 
+all_links = []
+
 for link in soup.findAll("a"):
     if 'href' in link.attrs:
-        print(link.attrs['href'])
+        all_links.append(link.get('href'))
+
+# print(all_links)
+
+temp = []
+
+for link in soup.find_all("a", attrs={"class": "link link_hover"}):
+    if 'href' in link.attrs:
+        temp.append(link.get('href'))
+    
+print(temp)
