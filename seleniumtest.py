ec589029b705be92ec77ad07f5f9604f92e44c7a
sophiafreuden <sfreuden@g.harvard.edu>
2020-12-10 13:05:53 -0500

    With selenium test script and basic selenium install directions.

---
+++ seleniumtest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 12:37:12 2020
+
+@author: sof565
+"""
+
+import selenium
+
+# If no errors pop up in the console, then this is working! If you're not
+# sure how to get selenium installed on your computer, try:
+# https://bit.ly/3m58mo9
+# This is a great YouTube tutorial series on selenium, which includes
+# installation and some debugging solutions.
+
