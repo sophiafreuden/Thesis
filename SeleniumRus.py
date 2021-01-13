de1735e1a1e7ec8fcd327c5901589326c6a1745a
sophiafreuden <sfreuden@g.harvard.edu>
2021-01-13 15:18:46 -0500

    Added functionality to SeleniumRus. Clicker and link scraper done.

--- SeleniumRus.py
+++ SeleniumRus.py
@@ -22,7 +22,7 @@ driver = webdriver.Chrome(PATH)
 
 # Change the URL below to what you need it to be.
 
-driver.get("https://russian.rt.com/search?q=%D1%81%D0%BE%D1%80%D0%BE%D1%81&type=&df=&dt=2019-12-31")
+driver.get("https://russian.rt.com/search?q=%D0%B0%D0%BF%D0%B5%D0%BB%D1%8C%D1%81%D0%B8%D0%BD+%D1%84%D1%80%D1%83%D0%BA%D1%82&type=&df=&dt=2019-12-31")
 
 closepopup = driver.find_elements_by_xpath('.//a[@class="subscribe__close js-subscribe-close"]')
 
@@ -36,38 +36,56 @@ results = driver.find_element_by_xpath('.//p[@class="search-serp__total"]').text
 
 resultsno = int(results[-3:])
 
-print("You have pinged " + str(resultsno) + "results.")
+print("You have pinged " + str(resultsno) + " results.")
 
 if resultsno >= 300:
     print("Too many results. Try narrowing your search.")
     driver.quit()
     sys.exit()
 
-time.sleep(3)
+if resultsno == 0:
+    print("No results found. Chrome will close automatically and this script will halt.")
+    driver.quit()
+    sys.exit()
 
-button = driver.find_element_by_xpath('.//a[@class="button__item button__item_listing"]')
+def clicker(number):
+    clicked = 0
+    rawpages = number / 15
+    pages = math.ceil(rawpages)
+    print("Pages is " + str(pages) + ".")
+    clicks = pages - 1
+    print("Clicks is " + str(clicks) + ".")
+    while clicked < clicks:
+        time.sleep(3) # Usually 3, more if "More" button disappears in console
+        button = driver.find_element_by_xpath('.//a[@class="button__item button__item_listing"]')
+        time.sleep(3) # Usually 3, more if "More" button disappears in console
+        driver.execute_script("arguments[0].click();", button)
+        if clicked > clicks:
+            break
+        clicked += 1
+        print("Pages clicked = " + str(clicked) + ".")
+    print("Clicking completed successfully.")
 
-print("working so far 3")
+if resultsno > 15:
+    print("Enough results to click through.")
+    clicker(resultsno)
 
-time.sleep(3)
+if resultsno <= 15 and resultsno != 0:
+    print("10 or fewer results. No clicking necessary.")
 
-driver.execute_script("arguments[0].click();", button)
-
-print("working so far 4")
+time.sleep(1)
 
 links = []
 
 for a in driver.find_elements_by_xpath('.//a[@class="link link_color"]'):
     links.append(a.get_attribute('href'))
 
-time.sleep(3)
+time.sleep(1)
 
-print(links)
+for link in links:
+    print(link)
 
-# This is working as is. I think the next script I make will be the full version
-# (SeleniumRus).
+print(len(links))
 
-print("Driver will quit automatically. Bye bye!")
-
-driver.quit()
+# driver.quit()
 
